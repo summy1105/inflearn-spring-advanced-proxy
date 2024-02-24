878491c0460c3679e4efff7ccacb41b71072d882
summy1105 <dsm851105@gmail.com>
2024-02-24 20:10:25 +0900

    BeanPostProcessor(빈후처리기) & advisor

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -8,6 +8,8 @@ import hello.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
 import hello.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
 import hello.proxy.config.v3_proxy.ProxyFactoryConfigV1;
 import hello.proxy.config.v3_proxy.ProxyFactoryConfigV2;
+import hello.proxy.config.v4_postprocessor.BeanPostProcessorConfig;
+import hello.proxy.config.v5_autoproxy.AutoProxyConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -20,7 +22,9 @@ import org.springframework.context.annotation.Import;
 //@Import({InterfaceProxyConfig.class, ConcreteProxyConfig.class})
 //@Import(DynamicProxyBasicConfig.class)
 //@Import(DynamicProxyFilterConfig.class)
-@Import({ProxyFactoryConfigV1.class, ProxyFactoryConfigV2.class})
+//@Import({ProxyFactoryConfigV1.class, ProxyFactoryConfigV2.class})
+//@Import(BeanPostProcessorConfig.class)
+@Import(AutoProxyConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app.v3") //주의
 public class ProxyApplication {
 
